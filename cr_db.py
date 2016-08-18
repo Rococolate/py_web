768788e06ed7e88d9c2ab22f03812ce2da833259
Rococolate <chocolatebloom@gmail.com>
2016-08-18 15:50:52 +0800

    文件上传

---
+++ cr_db.py
@@ -0,0 +1,75 @@
+import sqlite3 as sql
+
+
+def initDB(filename=None):
+    """
+    连接数据库
+    :param filename:
+    :return:
+    """
+    global db, cursor
+    if not filename:
+        filename = 'contact.db'
+    try:
+        db = sql.connect(filename)
+        cursor = db.cursor()
+        # cursor.execute('PRAGMA Foreign_Keys=TRUE')
+    except:
+        print("Error connecting to", filename)
+        cursor = None
+        raise
+
+
+def create_table():
+    """
+    建立contact表
+    :return:
+    """
+    query = '''create TABLE contact(
+name VARCHAR(255) not null,phone VARCHAR(255) not null,qq VARCHAR(255) not null,address VARCHAR(255) not null,marriage
+VARCHAR(255) not null
+)'''
+    cursor.execute(query)
+
+
+def update_person(name, phone, qq, address, marriage):
+    """
+    插入数据
+    :param name:
+    :param phone:
+    :param qq:
+    :param address:
+    :param marriage:
+    :return:
+    """
+    query = '''insert into contact values ('%s','%s','%s','%s','%s')''' % (name, phone, qq, address, marriage)
+    cursor.execute(query)
+    # print(query)
+
+
+def closeDB():
+    """
+    关闭数据库连接
+    :return:
+    """
+    try:
+        cursor.close()
+        db.commit()
+        db.close()
+    except:
+        print("problem closing database...")
+        raise
+
+
+def select_date():
+    query = '''select * from contact'''
+    return cursor.execute(query).fetchall()
+
+
+if __name__ == "__main__":
+    initDB()
+    # update_person("张三",16355466773,475883,"广州湛江","单身")
+    # closeDB()
+    # el = select_date()
+    # for i in el:
+    #     print(*i)
---
+++ my_web1.py
@@ -0,0 +1,75 @@
+import sqlite3, os
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+    render_template, flash
+import cr_db as db
+from time import sleep
+
+app = Flask(__name__)
+sys_path = os.getcwd()
+
+print(sys_path)
+app.config.update(dict(
+    DATABASE=os.path.join(app.root_path, 'contact.db'),
+    DEBUG=True,
+    SECRET_KEY='nickknackpaddywhack',
+    USERNAME='admin',
+    PASSWORD='thisisterrible'
+))
+app.config.from_envvar('LENDY_SETTINGS', silent=True)
+
+
+@app.route('/')
+@app.route('/adduser', methods=['GET', 'POST'])
+def add_user():
+    error = None
+    if request.method == 'POST':
+        name = request.form['name']
+        phone = request.form['phone']
+        qq = request.form['qq']
+        address = request.form['address']
+        marriage = request.form['marriage']
+        db.initDB()
+        db.update_person(name=name, phone=phone, qq=qq, address=address, marriage=marriage)
+        db.closeDB()
+        print(name, phone, qq, address, marriage)
+        return redirect(url_for('success'))
+    return render_template('adduser.html', error=error)
+
+
+@app.errorhandler(404)
+def file_not_found(error):
+    return render_template('404.html'), 404
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
+@app.route('/test')
+def test():
+    return render_template('mp4_player.html')
+
+
+@app.route('/userlist')
+def user_list():
+    db.initDB()
+    all_user_list = db.select_date()
+    all_user_number = len(all_user_list)
+    return render_template('userlist.html',
+                           all_user_list=all_user_list,
+                           all_user_number=all_user_number)
+
+@app.route('/listtest')
+def listtest():
+    db.initDB()
+    all_user_list = db.select_date()
+    all_user_number = len(all_user_list)
+    return render_template('listtest.html',
+                           all_user_list=all_user_list,
+                           all_user_number=all_user_number)
+
+if __name__ == '__main__':
+    # db.initDB()
+    # print(db.select_date())
+    app.run(host='0.0.0.0', port=80)
